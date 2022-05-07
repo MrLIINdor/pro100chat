ae4b0cb74d87c934cbc57451bac103a6c9657c2c
DaniBadalyan <liindor54@mail.ru>
2022-05-07 22:55:41 +0300

    Update navigate.js

---
+++ src/context/ReqContext.js
@@ -0,0 +1,34 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import {LocalContext} from '../../src/localContext.js';
+import axios from 'axios';
+
+
+export default function ReqContext() {
+
+
+    const register = (email, password) => {
+        axios
+            .post('https://pro100chat.herokuapp.com/api/v1/auth/signup', {
+                "firstName": "Krytou",
+                "lastName": "Ychitel",
+                "middleName": "Onidzuko",
+                "password": password,
+                "username": email
+            })
+    }
+
+    const login = (email, password) => {
+        axios
+            .post('https://pro100chat.herokuapp.com/api/v1/auth/signin', {
+                "password": password,
+                "username": email
+            })
+    }
+
+  return (
+    <View>
+      <Text>ReqContext</Text>
+    </View>
+  )
+}
